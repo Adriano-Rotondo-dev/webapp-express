fd5c28eacf424371227ff930c50db701bd7645a5
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-31 16:58:58 +0200

    connected to webapp_express database

---
+++ backend-api/database/connection.js
@@ -0,0 +1,20 @@
+//create db connection
+const mysql = require("mysql2");
+
+const credentials = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+const connection = mysql.createConnection(credentials);
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.info("✔ Connection successful");
+});
+
+module.exports = connection;
--- backend-api/server.js
+++ backend-api/server.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 
+//! app.use per asset statici, router, error handlers
+
 app.listen(PORT, () => {
   console.log(`Server is listening http://127.0.0.1:${PORT}`);
+});
+
+app.get(`/`, (req, res) => {
+  res.send("welcome to the webapp");
+});
+
+//endpoint index
+app.get(`/movies`, (req, res) => {
+  res.send("show movies inside db");
+});
+
+//endpoint show
+app.get(`/movies/:id`, (req, res) => {
+  res.send(`show movie ${req.params.id}`);
 });
